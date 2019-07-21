b19cc4f7d0ee5b2158634d3a8f94314c7adfebb5
khal745 <49116468+khal745@users.noreply.github.com>
2019-07-21 14:40:46 +1200

    added test project

---
+++ ScribrAPIUnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScribrAPIUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
